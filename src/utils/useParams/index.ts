dcc5b558fbf287f858fc7cef69cc50d8686a19ff
Tobias Zucali <tobias@zucali.at>
2025-12-29 14:09:01 +0100

    useParams: do not write unset params

--- src/utils/useParams/index.ts
+++ src/utils/useParams/index.ts
@@ -1,41 +1,64 @@
-import { useCallback, useMemo } from 'react';
-import { useSearchParams, usePathname, useRouter } from 'next/navigation';
+import { useCallback, useMemo } from "react";
+import { useSearchParams, usePathname, useRouter } from "next/navigation";
 
 export default function useParams() {
   const searchParams = useSearchParams();
-  const pathname = usePathname()
+  const pathname = usePathname();
   const router = useRouter();
 
   const isSearchParamsEmpty = searchParams.size === 0;
 
-  const params = useMemo(() => ({
-    m: searchParams.get('m') || '01',
-    s: searchParams.get('s') || '00',
-    title: searchParams.get('title') || '',
-    bg: searchParams.get('bg') || '#000000',
-    fg: searchParams.get('fg') || '#ffffff',
-    p: searchParams.get('p') || '#d61f69',
-    r: searchParams.get('r') || "", // remote peer id
-    c: searchParams.get('c') || "", // client peer id
-  }), [searchParams]);
+  const params = useMemo(
+    () => ({
+      m: searchParams.get("m") || "01",
+      s: searchParams.get("s") || "00",
+      title: searchParams.get("title") || "",
+      bg: searchParams.get("bg") || "#000000",
+      fg: searchParams.get("fg") || "#ffffff",
+      p: searchParams.get("p") || "#d61f69",
+      r: searchParams.get("r") || "", // remote peer id
+      c: searchParams.get("c") || "", // client peer id
+    }),
+    [searchParams]
+  );
 
-  const getPathWithParams = useCallback((newPathName = pathname, newParams = {}) => {
-    const newSearchParams = new URLSearchParams({
-      ...params,
-      ...newParams,
-    });
-    return newPathName + '?' + newSearchParams.toString()
-  }, [params, pathname]);
+  const getPathWithParams = useCallback(
+    (newPathName = pathname, newParams = {}) => {
+      const newSearchParams = new URLSearchParams();
 
-  const getUrlWithParams = useCallback((newPathName?: string, newParams = {}) => {
-    if (typeof window === 'undefined') {
-      return getPathWithParams(newPathName, newParams);
-    }
-    return new URL(getPathWithParams(newPathName, newParams), window.location.origin).toString();
-  }, [getPathWithParams]);
+      const mergedParams = { ...params, ...newParams };
+
+      (Object.keys(mergedParams) as Array<keyof typeof mergedParams>).forEach(
+        (key) => {
+          if (mergedParams[key]) {
+            newSearchParams.set(key, mergedParams[key]);
+          }
+        }
+      );
+
+      return newPathName + "?" + newSearchParams.toString();
+    },
+    [params, pathname]
+  );
+
+  const getUrlWithParams = useCallback(
+    (newPathName?: string, newParams = {}) => {
+      if (typeof window === "undefined") {
+        return getPathWithParams(newPathName, newParams);
+      }
+      return new URL(
+        getPathWithParams(newPathName, newParams),
+        window.location.origin
+      ).toString();
+    },
+    [getPathWithParams]
+  );
 
   const setParams = useCallback(
-    (newParams: Record<string, string>, push: boolean = isSearchParamsEmpty) => {
+    (
+      newParams: Record<string, string>,
+      push: boolean = isSearchParamsEmpty
+    ) => {
       const newUrl = getPathWithParams(undefined, newParams);
       if (push) {
         router.push(newUrl);
@@ -44,12 +67,22 @@ export default function useParams() {
       }
     },
     [getPathWithParams, router, isSearchParamsEmpty]
-  )
-  return useMemo(() => ({
-    params,
-    setParams,
-    getPathWithParams,
-    getUrlWithParams,
-    isSearchParamsEmpty,
-  }), [params, setParams, getPathWithParams, getUrlWithParams, isSearchParamsEmpty]);
+  );
+
+  return useMemo(
+    () => ({
+      params,
+      setParams,
+      getPathWithParams,
+      getUrlWithParams,
+      isSearchParamsEmpty,
+    }),
+    [
+      params,
+      setParams,
+      getPathWithParams,
+      getUrlWithParams,
+      isSearchParamsEmpty,
+    ]
+  );
 }
